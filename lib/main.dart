b02e8180cf34c82d246442f0cc56d56f9fba0908
Isabel Zacarias <isabel.zzacarias@gmail.com>
2026-02-25 17:53:19 -0600

    se agrego un inicio de la app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'theme/app_theme.dart';
+import 'screens/start_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/create_account_screen.dart';
 import 'screens/dashboard_screen.dart';
@@ -22,8 +23,9 @@ class NorthwestBankApp extends StatelessWidget {
       title: 'NorthwestBank',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      initialRoute: '/login',
+      initialRoute: '/start',
       routes: {
+        '/start': (_) => StartScreen(),
         '/login': (_) => LoginScreen(),
         '/account': (_) => CreateAccountScreen(),
         '/dashboard': (_) => const DashboardScreen(),
